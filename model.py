e4668e998d29864bb08cd166d1b5d36908787b48
Gabriel Nascarella Hishida <ielnascarella@hotmail.com>
2025-03-18 19:30:27 +0100

    initial idea and conception of models and training algo

---
+++ model.py
@@ -0,0 +1,41 @@
+import torchvision
+from torchvision.models import (resnet50, ResNet50_Weights,
+                                resnet18, ResNet18_Weights,
+                                mobilenet_v2, MobileNet_V2_Weights,
+                                mobilenet_v3_large, MobileNet_V3_Large_Weights
+                                )
+import torch
+import torch.nn as nn
+import math
+
+class SlayNet(nn.Module):
+
+    def __init__(self, inputsize=(250), embedding_size=(512)):
+        super().__init__()
+
+        self.backbone = mobilenet_v2(weights=MobileNet_V2_Weights.IMAGENET1K_V2).features
+
+        linear_input_size = 1280 * (math.ceil(inputsize/32) ** 2)
+        # mobilenet was designed with 32x32 in mind. shouldn't be too much of an issue.
+
+        self.linear = nn.Linear(linear_input_size, embedding_size)
+
+    def forward(self, x):
+
+        batchsize = x.shape[0]
+
+        x = self.backbone(x)
+        x = x.reshape(batchsize, -1) # flatten
+        x = self.linear(x)
+
+        return x
+    
+if __name__ == "__main__":
+    model = SlayNet()
+    print(model.backbone)
+
+    inp = torch.rand((1,3,250,250))
+    print(inp.shape)
+
+    out = model(inp)
+    print(out.shape)
---
+++ train.py
@@ -0,0 +1,59 @@
+from model import SlayNet
+import torch.nn as nn
+import torch
+from torch.optim.adam import Adam
+
+from lfw_dataloader import get_lfw_dataloaders
+
+# Create dataloaders
+train_loader, test_loader, num_classes = get_lfw_dataloaders(
+    "../", batch_size=8, blur_sigma=3
+)
+
+print(f"Dataset loaded successfully with {num_classes} unique individuals")
+print(f"Training batches: {len(train_loader)}, Test batches: {len(test_loader)}")
+
+if __name__ == "__main__":
+
+    lr = 2e-4
+    epochs = 20
+    epslon = 1e-6
+    criterion = nn.BCELoss()
+
+    blurnet = SlayNet(inputsize=250, embedding_size=512)
+    sharpnet = SlayNet(inputsize=250, embedding_size=512)
+
+    optimizer = Adam(params=[blurnet.parameters() + sharpnet.parameters()], lr=lr)
+
+
+    for epoch in range(epochs):
+
+        for batch_num, data in enumerate(zip(train_loader)):
+            # TODO need to work on this.
+            # either redesign dataloader or apply different shuffles to the same dataloader...
+
+            image_sharp, label_sharp, image_blur, label_blur = data
+
+            optimizer.zero_grad()
+            
+            embed_blur = blurnet(image_blur)
+            embed_sharp = sharpnet(image_sharp)
+
+            # loss is similar to CLIP
+            # taken from https://www.reddit.com/r/MLQuestions/comments/10ohrqo/understanding_the_clip_loss_function/
+
+            # similarity between embedding using dot product
+                # normalize embeddings
+            embed_blur = embed_blur / (embed_blur.norm() + epslon)
+            embed_sharp = embed_sharp /  (embed_sharp.norm() + epslon)
+
+            similarity = torch.dot(embed_blur, embed_sharp)
+
+            is_same_person = torch.equal(label_blur, label_sharp)
+
+            loss = criterion(similarity, is_same_person)
+            loss.backward()
+            optimizer.step()
+
+
+
