9659f6f89d7fce58a53a43905be2d7a89c3b05d3
Gabriel Hishida <gabrielnhn@duck.com>
2025-03-29 03:24:14 +0100

    training configs

--- train.py
+++ train.py
@@ -7,20 +7,6 @@ from datetime import datetime
 from torchvision import transforms
 
 from lfw_triple_loaders import get_lfw_dataloaders
-# from lfw_dataloader import get_lfw_dataloaders
- # # Create dataloaders
-# train_loader, test_loader, num_classes = get_lfw_dataloaders(
-#     "../lfw", batch_size=8, blur_sigma=3
-# )
-# eprint(f"Dataset loaded successfully with {num_classes} unique individuals")
-# eprint(f"Training batches: {len(train_loader)}, Test batches: {len(test_loader)}")
-
-# for the celebA dataset
-# from celebA_dataloader.dataset import CelebADual
-
-# face_transform = transforms.Compose([
-#     transforms.GaussianBlur(kernel_size=15, sigma=(10, 20)),  # Apply Gaussian blur with random sigma
-# ])
 
 import sys
 
@@ -31,11 +17,7 @@ device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cp
 
 if __name__ == "__main__":
 
-    # dualdataset = CelebADual(faceTransform=face_transform, dims=128, faceFactor=0.7, basicCrop=True)
-
-    ba_train_loader, ba_test_loader, _ = get_lfw_dataloaders("./data/lfw", batch_size=16, blur_sigma=3, anchor_blur=True)
-    sa_train_loader, sa_test_loader, _ = get_lfw_dataloaders("./data/lfw", batch_size=16, blur_sigma=3, anchor_blur=False)
-
+    train_loader, test_loader, _ = get_lfw_dataloaders("./data/lfw", batch_size=10, blur_sigma=3)
 
     eprint("Datasets were loaded")
     
@@ -56,60 +38,40 @@ if __name__ == "__main__":
     for epoch in range(epochs):
 
         eprint(f"Epoch {epoch}")
-
         losses = []
 
+        for idx, (anchor_1_sharp, anchor_2_blur, positive_1_blur, positive_2_sharp
+                ), in enumerate(train_loader):
+      
+            # anchor 1 sharp = negative 1 sharp
+            # negative 2 blur = anchor 2 blur
 
-        # for (image_sharp, label_sharp), (image_blur, label_blur) in dualdataset:
-        for idx, (
-            (anchor_sharp, positive_blur, negative_blur),
-            (anchor_blur, positive_sharp, negative_sharp)
-            ) in enumerate(zip(ba_train_loader, sa_train_loader)):
-            
-            anchor_sharp = anchor_sharp.to(device)
-            positive_blur = positive_blur.to(device)
-            negative_blur = negative_blur.to(device)
-            anchor_blur = anchor_blur.to(device)
-            positive_sharp = positive_sharp.to(device)
-            negative_sharp = negative_sharp.to(device)
+            anchor_1_sharp = anchor_1_sharp.to(device)
+            anchor_2_blur = anchor_2_blur.to(device)
+            positive_1_blur = positive_1_blur.to(device)
+            positive_2_sharp = positive_2_sharp.to(device)
+
 
             optimizer.zero_grad()
             
-            # train sharp network
             sharpnet.train()
-
-            # eprint("Training sharp")
-
-            # eprint("ANCHOR SHARP SHAPE", anchor_sharp.shape)
-
-            anchor_sharp_embed = sharpnet(anchor_sharp)
-            # eprint("Got embedding for sharp")
-            with torch.no_grad():
-                blurnet.eval()
-                positive_blur_embed = blurnet(positive_blur)
-                # eprint("Got embedding for posblur")
-                negative_blur_embed = blurnet(negative_blur)
-                # eprint("Got embedding for negblur")
-            
-            sharp_loss = triplet_loss(anchor_sharp_embed, positive_blur_embed, negative_blur_embed)
-            # eprint("Got tripletloss")
-
-
-            # train blur network
             blurnet.train()
 
 
-            anchor_blur_embed = blurnet(anchor_blur)
-            # eprint("Got embedding for blur anchor")
-            with torch.no_grad():
-                sharpnet.eval()
-                positive_sharp_embed = sharpnet(positive_sharp)
-                # eprint("Got embedding for possharp")
-                negative_sharp_embed = sharpnet(negative_sharp)
-                # eprint("Got embedding for negsharp")
+            # train sharp network
+            anchor_sharp_embed = sharpnet(anchor_1_sharp)
+            positive_blur_embed = blurnet(positive_1_blur)
+            negative_blur_embed = blurnet(anchor_2_blur)
+            
+            sharp_loss = triplet_loss(anchor_sharp_embed, positive_blur_embed, negative_blur_embed)
+
+
+            # train blur network
+            anchor_blur_embed = negative_blur_embed
+            positive_sharp_embed = sharpnet(positive_2_sharp)
+            negative_sharp_embed = anchor_sharp_embed
             
             blur_loss = triplet_loss(anchor_blur_embed, positive_sharp_embed, negative_sharp_embed)
-            # eprint("Got tripletloss")
 
             loss = (blur_loss + sharp_loss)
             
@@ -118,7 +80,6 @@ if __name__ == "__main__":
             lossval = loss.item()
             losses.append(lossval)
             print(lossval, end=", ", flush=True)
-            # print("Lossval is: ", lossval)
 
             optimizer.step()
 
