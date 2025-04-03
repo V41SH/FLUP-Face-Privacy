b3afd0da33414af21a56006010ef076ec8b4d140
Gabriel Hishida <gabrielnhn@duck.com>
2025-04-04 01:06:25 +0200

    add plots!!

--- plot_results.py
+++ plot_results.py
@@ -1,41 +1,58 @@
 import matplotlib.pyplot as plt
 
-with open("blur_evaluation_results/final_results.txt") as f:
-    lines = f.readlines()
 
-for _ in range(2):
-    lines.pop(0)
+files = ["eval_results/eval_tight.txt", "eval_results/eval_arcface.txt"] 
 
-label = lines.pop(0).split(": ")[1]
+model_vals = {}
 
-same_person = {}
-diff_person = {}
-all_pairs = {}
+for file in files:
+    model_vals[file] = {}
+    with open(file) as f:
+        lines = f.readlines()
 
-titles = ["Same Person Evaluation",
-"Different Person Evaluation",
-"All Pairs Evaluation"]
+    for _ in range(2):
+        lines.pop(0)
 
+    label = lines.pop(0).split(": ")[1]
 
+    same_person = {}
+    diff_person = {}
+    all_pairs = {}
 
+    titles = ["Same Person Evaluation",
+    "Different Person Evaluation",
+    "All Pairs Evaluation"]
 
-while lines:
-    line = lines.pop(0)
-    print(line)
-    line = line.strip()[:-1]
-    _, blur_level = line.split()
-    for metric in (same_person, diff_person, all_pairs):
+    while lines:
         line = lines.pop(0)
-        val = float(line.split("= ")[-1])
-        metric[blur_level] = val
-    
+        print(line)
+        line = line.strip()[:-1]
+        _, blur_level = line.split()
+        for metric in (same_person, diff_person, all_pairs):
+            line = lines.pop(0)
+            val = float(line.split("= ")[-1].split("(")[0])
+            metric[blur_level] = val
+        
+    model_vals[file]["same"] = same_person
+    model_vals[file]["diff"] = diff_person
+    model_vals[file]["all"] = all_pairs
+    model_vals[file]["label"] = str(label)
 
-for idx, metric in enumerate((same_person, diff_person, all_pairs)):
-    plt.plot(metric.keys(), metric.values(), marker='o', label=label)
+# for idx, metric in enumerate((same_person, diff_person, all_pairs)):
+
+
+for idx, metric in enumerate(("same", "diff", "all")):
+
+    for file in files:
+        plt.plot(model_vals[file][metric].keys(), model_vals[file][metric].values(),
+                 marker='o', label=model_vals[file]["label"])
+
+    
     plt.xlabel('Blur Sigma')
-    # plt.ylabel('Cosine Similarity' if category == 'same' else '1 - Cosine Similarity')
-    plt.ylabel('Cosine Similarity')
+    plt.ylabel('Cosine Similarity' if metric != 'diff' else '1 - Cosine Similarity')
     plt.title(titles[idx])
     plt.legend()
     plt.grid(True)
+    plt.ylim(-0.2,1.2)
+
     plt.show()
